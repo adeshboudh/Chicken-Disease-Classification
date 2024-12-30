5717f4fe6bc5be8d9e358706d4d9de51a1658a94
AdeshBoudh <adeshboudh16@gmail.com>
2024-12-30 14:07:36 +0530

    requirements added

--- template.py
+++ template.py
@@ -18,12 +18,12 @@ list_of_files = [
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
+    "templates/index.html"
     "dvc.yaml",
     "params.yaml",
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "templates/index.html"
 ]
 
 
