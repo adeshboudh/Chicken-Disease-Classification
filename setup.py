c343a5e2b5b087da034030e6cc1870d64a507fcc
AdeshBoudh <adeshboudh16@gmail.com>
2025-01-14 11:01:25 +0530

    requirements added

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Chicken-Disease-Classification--Project"
-AUTHOR_USER_NAME = "AdeshBoudh"
+REPO_NAME = "Chicken-Disease-Classification-Project"
+AUTHOR_USER_NAME = "adeshboudh"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "adeshboudh16@gmail.com"
 
